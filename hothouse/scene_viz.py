c87bcd65d5e6d7666280f1945f8511b93df7345e
Matthew Turk <matthewturk@gmail.com>
2020-07-08 14:50:23 -0500

    link blaster to viz

--- hothouse/scene_viz.py
+++ hothouse/scene_viz.py
@@ -5,6 +5,7 @@ import numpy as np
 import traitlets
 import traittypes
 from .scene import Scene
+from .blaster import RayBlaster
 
 
 def _get_cmap_texture(cmap_name):
@@ -22,6 +23,8 @@ class CropRenderer(traitlets.HasTraits):
     renderer = traitlets.Instance(pythreejs.Renderer)
     camera = traitlets.Instance(pythreejs.Camera)
     controls = traitlets.List()
+    blaster = traitlets.Instance(RayBlaster)
+    # arrow = traitlets.Instance()
 
     @traitlets.default("renderer")
     def _renderer_default(self):
@@ -69,3 +72,29 @@ class CropRenderer(traitlets.HasTraits):
     @traitlets.default("controls")
     def _controls_default(self):
         return [pythreejs.OrbitControls(controlling=self.camera)]
+
+    def update_blaster(self, change=None):
+        counts = self.base_scene.compute_hit_count(self.blaster)
+        max_val = max(_.max() for _ in counts.values())
+        for i, count in sorted(counts.items()):
+            nv = self.base_scene.components[i].values * 0.0
+            c = count / max_val
+            nv[0::3, 0] = c
+            nv[1::3, 0] = c
+            nv[2::3, 0] = c
+            self.base_scene.components[i].values = nv
+
+    @traitlets.observe("blaster")
+    def _blaster_changed(self, change):
+        # Disconnect our old blaster
+        if isinstance(change["old"], RayBlaster):
+            change["old"].unobserve(self.update_blaster, traitlets.All)
+        change["new"].observe(
+            self.update_blaster,
+            ["directions", "origins", "intensity", "diffuse_intensity"],
+        )
+        self.update_blaster()
+
+    @traitlets.default("arrow")
+    def _arrows_default(self, change):
+        pass
